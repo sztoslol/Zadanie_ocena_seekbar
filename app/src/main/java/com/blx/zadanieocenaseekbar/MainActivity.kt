2e94e82487e6526f50904934bfc71048d5ea16bb
sztoslol <kubuszeski05@wp.pl>
2023-02-07 09:11:40 +0100

    dodanie mechaniki skalowania oraz poprawki błędów

--- app/src/main/java/com/blx/zadanieocenaseekbar/MainActivity.kt
+++ app/src/main/java/com/blx/zadanieocenaseekbar/MainActivity.kt
@@ -24,5 +24,119 @@ class MainActivity : AppCompatActivity() {
         var jablko = findViewById<ImageView>(R.id.imageView_1_jabka)
         var granat = findViewById<ImageView>(R.id.imageView_2_granat)
         var gruszka = findViewById<ImageView>(R.id.imageView_3_gruszki)
-    }
+
+        var max = 300
+        var min = 1
+
+
+//<============================== Jabko ==============================>
+        seekbar_jablko_h?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                jablko.scaleX = (seek.progress/100).toFloat()
+                progressBar_h.progress = seekbar_granat_h.progress + seekbar_gruszka_h.progress + seekbar_jablko_h.progress
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+        seekbar_jablko_v?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                jablko.scaleY = (seek.progress/100).toFloat()
+                progressBar_v.progress = seekbar_granat_v.progress + seekbar_gruszka_v.progress + seekbar_jablko_v.progress
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+//<============================== Granat ==============================>
+
+        seekbar_granat_h?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                granat.scaleX = (seek.progress/100).toFloat()
+                progressBar_h.progress = seekbar_granat_h.progress + seekbar_gruszka_h.progress + seekbar_jablko_h.progress
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+        seekbar_granat_v?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                granat.scaleY = (seek.progress/100).toFloat()
+                progressBar_v.progress = seekbar_granat_v.progress + seekbar_gruszka_v.progress + seekbar_jablko_v.progress
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+//<============================== Gruszka ==============================>
+
+        seekbar_gruszka_h?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                gruszka.scaleX = (seek.progress/100).toFloat()
+                progressBar_h.progress = seekbar_granat_h.progress + seekbar_gruszka_h.progress + seekbar_jablko_h.progress
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+        seekbar_gruszka_v?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                gruszka.scaleY = (seek.progress/100).toFloat()
+                progressBar_v.progress = seekbar_granat_v.progress + seekbar_gruszka_v.progress + seekbar_jablko_v.progress
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+}
+}
+fun Refresh_ProgressBar() : Unit{
+
 }
